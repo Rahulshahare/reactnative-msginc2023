c647389e2664d10370ec617f78e7de80bef4279e
Rahul Shahare <rahulshahare@gmail.com>
2023-02-11 00:50:01 +0530

    register state for devtool

--- store/Store.js
+++ store/Store.js
@@ -1,7 +1,12 @@
-import { Store } from "pullstate";
+import { registerInDevtools, Store } from "pullstate";
 
 export const MsgStore = new Store({
     isLoggedIn : false,
     userDetails: '',
     friendList: '',
+});
+
+
+registerInDevtools({
+    MsgStore,
 });
